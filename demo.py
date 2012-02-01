d55bc91abf2992dfbd487ffe857fd703d0786ad1
Marius <marius.kriegerowski@zmaw.de>
2012-02-01 13:21:31 +0100

    quick base for a map added

--- demo.py
+++ demo.py
@@ -2,16 +2,20 @@ import sys, time
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+import PyQt4
 
 import pyrocko.pile, pyrocko.pile_viewer, pyrocko.hamster_pile
+import pyrocko
 
 class MineDemo(QApplication):
 
 
     def __init__(self, args):
         QApplication.__init__(self, args)
-
+        
         self._win = QMainWindow()
+        self._win.setGeometry(50,0,800,900)
+        self._win.setWindowTitle("MINE")
         self._win.show()
 
 
@@ -19,6 +23,7 @@ class MineDemo(QApplication):
         
         button1 = QPushButton('Detection', self._win)
         button2 = QPushButton('Location', self._win)
+        self.connect(button2, SIGNAL('clicked()'), self.location)
         button3 = QPushButton('FocalMechanism', self._win)
         button4 = QPushButton('Statistics', self._win)
         button5 = QPushButton('Tomography', self._win)
@@ -26,7 +31,9 @@ class MineDemo(QApplication):
         self.connect(button6, SIGNAL('clicked()'), self.quit)
         
         infotext = QLabel('Long text giving information on the chosen application .................', self._win)
+        infotext.setStyleSheet("font: 18pt") 
         label = QLabel('Second Label', self._win)
+
         imagelogomine = QPixmap("./logomine_small.png")
         logomine = QLabel('', self._win)
         logomine.setPixmap(imagelogomine)
@@ -36,7 +43,7 @@ class MineDemo(QApplication):
         guititle = QLabel('MINE Project GUI Demonstrator', self._win)
         
         self.layout = QGridLayout()
-        
+         
         self.frame = QFrame(self._win)
         self.frame.setLayout(self.layout)
        
@@ -45,22 +52,22 @@ class MineDemo(QApplication):
         
         self.layout.addWidget(infotext, 0,0,1,9)
         self.layout.addWidget(self._pile_viewer, 1,0,1,9)
-        self.layout.addWidget(guititle, 2,0)
-        self.layout.addWidget(button1, 2,1)
-        self.layout.addWidget(button2, 2,2)
-        self.layout.addWidget(button3, 2,3)
-        self.layout.addWidget(button4, 2,4)
-        self.layout.addWidget(button5, 2,5)
-        self.layout.addWidget(button6, 2,6)
-        self.layout.addWidget(logomine, 2,7)
-        self.layout.addWidget(logogeotech, 2,8)
+        self.layout.addWidget(guititle, 3,0)
+        self.layout.addWidget(button1, 3,1)
+        self.layout.addWidget(button2, 3,2)
+        self.layout.addWidget(button3, 3,3)
+        self.layout.addWidget(button4, 3,4)
+        self.layout.addWidget(button5, 3,5)
+        self.layout.addWidget(button6, 3,6)
+        self.layout.addWidget(logomine, 3,7)
+        self.layout.addWidget(logogeotech, 3,8)
 
         self._win.setCentralWidget(self.frame)
 
     def start_pile_viewer(self, ntracks=5, use_opengl=False, panel_parent=None, follow=120):
         self._source_pile = pyrocko.pile.make_pile(['demodata.mseed'])
         self._tlast = time.time()
-        
+         
         p = pyrocko.hamster_pile.HamsterPile()
         p.set_fixation_length(20.)
         
@@ -71,7 +78,7 @@ class MineDemo(QApplication):
         
         self._timer = QTimer( self )
         self.connect( self._timer, SIGNAL("timeout()"), self.periodical ) 
-        self._timer.setInterval(3000)
+        self._timer.setInterval(4000)
         self._timer.start()
 
     def periodical(self):
@@ -95,8 +102,58 @@ class MineDemo(QApplication):
             shiftinsert(tmin, tmax, self._tlast-tmin)
         
         self._tlast = tnow
+    
+    def event_alert(self,t_detection):
+        event_ID = QLabel(t_detection, self._win)
+        
+        event_ID.setStyleSheet("background-color: rgb(255,0,0)")
+        
+        self.layout.addWidget(event_ID,2,4,1,1)
+
+    def location(self):
+        x_shift = 20
+        y_shift = 20
+        # create objects for drawing
+        dash_pen = QPen(QColor("black"))
+        b_brush = QBrush(QColor("black"))
+        r_brush = QBrush(QColor("red"))
+        triangle1 = QPolygonF()
+        triangle1.append(QPointF(10,-10))
+        triangle1.append(QPointF(0,5))
+        triangle1.append(QPointF(20,5))
+
+        triangle = QPolygonF()
+        triangle.append(QPointF(10+x_shift,-10+y_shift))
+        triangle.append(QPointF(0+x_shift,5+y_shift))
+        triangle.append(QPointF(20+x_shift,5+y_shift))
+
+        #create canvas for overview map and add to layout:
+        map_canvas = QGraphicsView()
+        self.layout.addWidget(map_canvas,1,1)
+
+        self.loc_map = QGraphicsScene()
+        map_canvas.setScene(self.loc_map)
+
+        # Data do be drawed:
+        self.scene_data2 = []
+        self.scene_data = []
+        self.scene_data.append({'routine':self.loc_map.addPolygon,
+                                'z':1,
+                                'args':(triangle,dash_pen,b_brush)})
+        self.scene_data2.append({'routine':self.loc_map.addPolygon,
+                                'z':2,
+                                'args':(triangle1,dash_pen,r_brush)})
+        # draw Data:
+        d = self.scene_data.pop(0)
+        item = d['routine'](*d['args'])
+        item.setZValue(d['z'])      # setZValue sets stacking order of items
+
+        d = self.scene_data2.pop(0)
+        item = d['routine'](*d['args'])
+        item.setZValue(d['z'])      # setZValue sets stacking order of items
 
 args = sys.argv
 minedemo = MineDemo(args)
+minedemo.event_alert('latest Event: '+'234')
 minedemo.exec_()
 
